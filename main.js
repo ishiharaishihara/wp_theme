82412efabdbf324277f92d8e418ca64a417a9f6a
ishiharaishihara <ishihara12345678@gmail.com>
2017-10-12 15:49:30 +0900

    init

---
+++ main.js
@@ -0,0 +1,31 @@
+$(function()
+{
+    $.getJSON('./flower.json',function(data)
+    {
+        var len = data.length;
+        var post = $('.posts')
+        post.append('<div class="row"></div>');
+            for(var i = 0; i < len; i++)
+            {
+                var k = (i+1)%4;
+                if( k == 0)
+                {
+                    var post = $('.posts')
+                    post.append('<div class="row"></div>');
+                }
+                var row = $('.row:last');
+                row.append(
+                        $('<figure/>')
+                            .attr({'class':'col-sm-4'})
+                            .append(
+                                $('<img/>')
+                                .attr({'src': data[i].img})
+                                )
+                            .append(
+                                $('<figcaption/>')
+                                .append('<h3>'+ data[i].title +'</h3>'+data[i].text)
+                                )
+                        );
+            }
+    });
+});
