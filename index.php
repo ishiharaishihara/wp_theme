c396ddb762cca9ea2e20c6ab18174b1f8d232b69
ishiharaishihara <ishihara12345678@gmail.com>
2017-10-16 10:40:41 +0900

    index修正

--- index.php
+++ index.php
@@ -11,6 +11,39 @@
             <h2>The latests blog posts</h2>
             Filter by category:All - Awesome - Fantastic - Non classe - Wonderfl - Reset filter
             <div class="posts">
+                    <div class="row">
+                        <figure class = "col-sm-4">
+                            <img src = "<?php bloginfo('template_directory'); ?>/imgs/flower.jpg">
+                            <figcaption>
+                                <h3>Sample post</h3>
+                                I can't abide these Jawas.Disgusting creatures.Go on, go on.
+                            </figcaption>
+                        </figure>
+                        <figure class = "col-sm-4">
+                            <img src = "<?php bloginfo('template_directory'); ?>/imgs/flower.jpg">
+                            <figcaption>
+                                <h3>This is a third blog article</h3>
+                                This is the third article excerpt.
+                            </figcaption>
+                        </figure>
+                        <figure class = "col-sm-4">
+                            <img src = "<?php bloginfo('template_directory'); ?>/imgs/flower.jpg">
+                            <figcaption>
+                                <h3>This is a second blog article</h3>
+                                This is the second article excerpt.
+                            </figcaption>
+                        </figure>
+                    </div>
+                    <div class="row">
+                        <figure class = "col-sm-4">
+                            <img src="<?php bloginfo('template_directory'); ?>/imgs/flower.jpg">
+                            <figcaption>
+                                <h3>This is a sample blog article</h3>
+                                This is the first article excerpt.
+                            </figcaption>
+                        </figure>
+                    </div>
             </div>
         </div>
         <?php get_footer(); ?>
+
